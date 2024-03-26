d4e59f78585619cdadf86157d7a65d3e5331b71f
Lera <valeriiakolesnikova21@gmail.com>
2024-03-26 18:01:25 +0500

    2 lesson modified

--- lesson3/src/App.js
+++ lesson3/src/App.js
@@ -1,6 +1,52 @@
 import React, { useState } from 'react';
 import './App.css';
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function App() {
   const text = 'Делаю первую домашку по реакту'
   return (
@@ -11,3 +57,33 @@ function App() {
 }
 
 export default App;
+
+
+
+const SampleForVirtualDOM = ({data = []}) =>{
+  const theme = useTheme()
+  // console.log(theme)
+  console.log(data)
+
+return(
+  <div style = {{background:theme.palette.primary.main}}>
+    Какой-то текст
+    <br/>
+    {data}
+  </div>
+)
+}
+
+SampleForVirtualDOM.propTypes = {
+  data: PropTypes.array,
+}
+
+// SampleForVirtualDOM.defaultProps = {
+//  data:[]
+// }
+
+{/*<Button variant="contained">Outlined</Button>
+
+<FormGroup>
+  <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+</FormGroup> */}
